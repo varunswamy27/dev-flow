e4fa929f36ab21f611bc332e8aa7832279ad1c02
varunswamyting <varunswamy27@gmail.com>
2025-07-07 20:38:58 +0530

    Implement authentication layout and social login buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -41,7 +40,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,9 +84,15 @@ export default {
         "space-grotesk": ["var(--font-space-grotesk)"],
       },
       borderRadius: {
+        "2": "8px",
+        "1.5": "6px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      backgroundImage: {
+        "auth-dark": "url('/images/auth-dark.png')",
+        "auth-light": "url('/images/auth-light.png')",
       },
     },
   },

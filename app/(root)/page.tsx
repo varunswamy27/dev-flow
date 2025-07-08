89b9f6d640bfcababd89afdc6a38b8109610f7c2
varunswamyting <varunswamy27@gmail.com>
2025-07-08 20:48:52 +0530

    Implement authentication with NextAuth, add sign-in and sign-out functionality, and integrate toast notifications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,29 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constant/routes";
+
 /** Home Page Jsx*/
-export default function Home() {
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <>
       <h1 className="h1-bold text-primary-500">
         Welcome to the world of Next.Js
       </h1>
-      <h1 className="text-3xl text-primary-500 font-space-grotesk">
-        Welcome to the world of Next.Js
-      </h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
     </>
   );
-}
+};
+
+export default Home;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,12 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { toast } from "@/hooks/use-toast";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constant/routes";
 
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log("Error during sign-in:", error);
+      toast({
+        title: "Sign-in Failed",
+        description:
+          error instanceof Error
+            ? error.message
+            : "An unexpected error occurred.",
+        variant: "destructive",
+      });
+    }
+  };
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src="/icons/github.svg"
           alt="GitHub Icon"
@@ -16,7 +40,7 @@ const SocialAuthForm = () => {
         />
         <span>Log in with Github</span>
       </Button>
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="Google Icon"

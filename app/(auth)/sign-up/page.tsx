27a325544d899b309a166ff8689a7c958afffb34
varunswamyting <varunswamy27@gmail.com>
2025-07-16 20:21:41 +0530

    WIP

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <AuthForm
+        formType="SIGN_UP"
+        schema={SignInSchema}
+        defaultValues={{ email: "", password: "" }}
+        onSubmit={(data) => Promise.resolve({ success: true, data })}
+      />
+    </div>
+  );
 };
 
 export default SignUp;
